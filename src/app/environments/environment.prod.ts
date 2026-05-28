9262f1cfe453406822d3a90a303fd1b4db4a5026
Emy <emily.galeas@epn.edu.ec>
2026-05-28 18:13:55 -0500

    actualizar rutas

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://khsaqilcuslsdsxkxjzl.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtoc2FxaWxjdXNsc2RzeGt4anpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MjA0NjcsImV4cCI6MjA5NDk5NjQ2N30.HW9SkO2wmYnLfDvelDjdR-W3QmWbw4KbYipLQyTekeA',
+  supabaseBucket: 'gadgets-assets',
+  firebase: {
+    apiKey: "AIzaSyDXPt7quzND3NoyLr7hWfDxaP8_DVh_feo",
+    authDomain: "videojuegos-403c3.firebaseapp.com",
+    projectId: "videojuegos-403c3",
+    storageBucket: "videojuegos-403c3.firebasestorage.app",
+    messagingSenderId: "547974760699",
+    appId: "1:547974760699:web:04eb5dc2f0b87c7ea7c85e",
+    measurementId: "G-2TQKBDTS8F"
+  }
+};
